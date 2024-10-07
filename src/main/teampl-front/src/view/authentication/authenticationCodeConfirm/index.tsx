533fe568f0508ac9ca36a6a93cc9de71424af61c
jang <jdj81204@naver.com>
2024-10-07 22:43:01 +0900

    메일인증 컴포넌트 완성

--- src/main/teampl-front/src/view/authentication/authenticationCodeConfirm/index.tsx
+++ src/main/teampl-front/src/view/authentication/authenticationCodeConfirm/index.tsx
@@ -1,9 +1,171 @@
 import "./style.css";
+import React, {ChangeEvent, useEffect, useRef, useState} from "react";
+import {KeyboardEvent} from "react";
 
-export default function ConfirmAuthCode(){
-    return(
+export default function ConfirmAuthCode() {
+    // state: 인증번호 input head 상태
+    const [codeHeads, setCodeHeads] = useState<string[]>(Array(3).fill(''));
+    // state: 인증번호 input body 상태
+    const [codeBodies, setCodeBodies] = useState<string[]>(Array(3).fill(''));
+
+    // reference: 인증 코드 input head 및 body refer
+    const headRefs = useRef<(HTMLInputElement | null)[]>([]);
+    const bodyRefs = useRef<(HTMLInputElement | null)[]>([]);
+
+
+    //eventHandler: 인증번호 input head 변경 이벤트처리 헨들러
+    const onCodeInputHeadsChangeEventHandler = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+        const value = e.target.value;
+
+        if (/^[0-9]$/.test(value) || value === '') {  //  유효성 검사 숫자인 경우에만 업데이트
+            // 배열의 얕은 복사
+            const newCodeHeads = [...codeHeads];
+            newCodeHeads[index] = value;
+            setCodeHeads(newCodeHeads);
+        }
+    };
+
+    const onCodeHeadsKeyDownEventHandler = (e: KeyboardEvent<HTMLInputElement>, index: number) => {
+        if (e.key ==="Backspace"){
+            // 현재 인풋이 비어있지 않은 경우
+            if (codeHeads[index] === "") {
+                // 이전 인풋으로 포커스 이동
+                if (index > 0) {
+                    headRefs.current[index - 1]?.focus();
+                }
+            }
+        }
+
+    }
+
+    const onCodeBodiesKeyDownEventHandler = (e: KeyboardEvent<HTMLInputElement>, index: number) => {
+        if(e.key == "Backspace"){
+            if (codeBodies[index] ===""){
+                if(index > 0)
+                    bodyRefs.current[index-1]?.focus();
+                else if(index == 0)
+                    headRefs.current[codeHeads.length-1]?.focus();
+            }
+        }
+    }
+
+
+    //eventHandler: 인증번호 input body 변경 이벤트처리 헨들러
+    const onCodeInputBodiesChangeEventHandler = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+        const value = e.target.value;
+
+        if (/^[0-9]$/.test(value) || value === '') {
+            //배열의 얕은 복사
+            const newCodeBodies = [...codeBodies];
+            newCodeBodies[index] = value;
+            setCodeBodies(newCodeBodies);
+
+        }
+
+    }
+
+
+    // interface: 인풋 엘리먼트 전달 Prop
+    interface CodeInputProp {
+        value: string,
+        onChange: (e: ChangeEvent<HTMLInputElement>) => void,
+        onKeyDown: (e: KeyboardEvent<HTMLInputElement>) => void;
+    }
+
+
+    const CodeInput =
+        React.forwardRef<HTMLInputElement, CodeInputProp>((props: CodeInputProp, ref) => {
+            const {value, onChange, onKeyDown} = props;
+            return (
+                <div>
+                    <div id={"confirm-code-input-wrapper"}>
+                        <input ref={ref} className={"code-confirm-code"} type={'text'} maxLength={1} value={value}
+                               onChange={onChange}
+                               onKeyDown={e => onKeyDown(e)}/>
+                    </div>
+
+                </div>
+            )
+        });
+
+    useEffect(() => {
+        // 가장 먼저 빈 값을 가진 인덱스 찾기 만일 조건에 부합하지 않는경우 -1을반환
+        const currentIndexCodeHeads = codeHeads.findIndex(code => code === "");
+        const currentIndexCodeBodies = codeBodies.findIndex(code => code ==="");
+        // 빈 값이 있는 경우 해당 인덱스로 포커스 이동
+        if (currentIndexCodeHeads > -1 && headRefs.current[currentIndexCodeHeads]) {
+
+            const currentHeadEle = headRefs.current[currentIndexCodeHeads];
+            if (currentHeadEle === null) return;
+            currentHeadEle.focus();
+
+        }else if(currentIndexCodeHeads === -1){
+
+            if(currentIndexCodeBodies > -1 && bodyRefs.current[currentIndexCodeBodies]){
+                const currentBodyEle = bodyRefs.current[currentIndexCodeBodies];
+                if (currentBodyEle === null) return;
+                currentBodyEle.focus();
+            }
+        }
+    }, [codeHeads, codeBodies]);
+
+    return (
         <div id={"code-confirm-card-wrapper"}>
             <div className={"code-confirm-card-container"}>
+                <div className={"code-confirm-top-container"}>
+
+                    <div className={"code-confirm-top-logo-container"}>
+                        <div className={"code-confirm-top-logo tool-logo"}></div>
+                        <div className={"code-confirm-top-tool-name"}>{"TeamPL"}</div>
+                    </div>
+
+                    <div className={"code-confirm-top-comment-container"}>
+                        <div className={"code-confirm-top-comment-title-container"}>
+                            <div className={"code-confirm-top-comment-title"}>{"이메일 인증 코드 입력"}</div>
+                        </div>
+
+
+                        <div className={"code-confirm-top-comment-content-container"}>
+                            <div className={"code-confirm-comment-content"}>{"" +
+                                "이메일로 전송받은 인증코드를 입력하시고 확인 버튼을 클릭해 주세요."}</div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className={"code-confirm-middle-container"}>
+                    <div className={"code-confirm-code-container"}>
+                        <div className={"code-confirm-code-head"}>
+
+                            {codeHeads.map((item, index) =>
+                                <CodeInput ref={el => (headRefs.current[index]) = el} key={index} value={item}
+                                           onChange={e => onCodeInputHeadsChangeEventHandler(e, index)}
+                                           onKeyDown={(e) => onCodeHeadsKeyDownEventHandler(e, index)}
+
+                                />)}
+                        </div>
+                        <div className={"code-confirm-code-body"}>
+
+                            {codeBodies.map((item, index) =>
+                                <CodeInput ref={el => bodyRefs.current[index] = el} key={index} value={item}
+                                           onChange={e => onCodeInputBodiesChangeEventHandler(e, index)}
+                                           onKeyDown={e => onCodeBodiesKeyDownEventHandler(e,index)}
+                                />)}
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"code-confirm-bottom-container"}>
+                    <div className={"code-confirm-bottom-comment-container"}>
+                        <div className={"code-confirm-bottom-comment"}>{"인증 코드가 발송되지 않았을 경우 '재전송' 버튼을 클릭하세요."}</div>
+                        <div className={"code-confirm-resend-code"}>{"인증 코드 재전송"}</div>
+                    </div>
+
+                    <div className={"code-confirm-button-container"}>
+                        <div className={"code-confirm-button"}>{"인증하기"}</div>
+                    </div>
+                </div>
 
             </div>
         </div>
