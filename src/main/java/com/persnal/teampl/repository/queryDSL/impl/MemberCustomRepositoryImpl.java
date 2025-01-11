7ec68ca622ffec3b887e3ddbb366ee685cc1d968
jang <jdj81204@naver.com>
2025-01-11 16:08:50 +0900

    큐 클래스 추가후 커밋

--- src/main/java/com/persnal/teampl/repository/queryDSL/impl/MemberCustomRepositoryImpl.java
+++ src/main/java/com/persnal/teampl/repository/queryDSL/impl/MemberCustomRepositoryImpl.java
@@ -7,6 +7,7 @@ import com.persnal.teampl.entities.QTeamMemberEntity;
 import com.persnal.teampl.entities.QUserEntity;
 import com.persnal.teampl.repository.queryDSL.MemberCustomRepository;
 import com.persnal.teampl.util.Utils;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -28,19 +29,20 @@ public class MemberCustomRepositoryImpl implements MemberCustomRepository {
             QTeamEntity team = QTeamEntity.teamEntity;
             QTeamMemberEntity teamMember = QTeamMemberEntity.teamMemberEntity;
 
-//
-//            list = queryFactory.select(Projections.constructor(TeamMemberObj.class,
-//                            teamMember.teamMemberPk,
-//                            user.email,
-//                            user.profileImg,
-//                            team.teamName
-//                    ))
-//                    .from(teamMember)
-//                    .innerJoin(user).on(teamMemberPk.email.eq(user.email))
-//                    .innerJoin(team).on(teamMemberPk.regNum.eq(team.regNum))
-//                    .fetchJoin()
-//                    .where(teamMemberPk.regNum.eq(regNum))
-//                    .fetch();
+
+
+            list = queryFactory.select(Projections.constructor(TeamMemberObj.class,
+                            teamMember.teamMemberPk.regNum,
+                            user.email,
+                            user.profileImg,
+                            team.teamName
+                    ))
+                    .from(teamMember)
+                    .innerJoin(user).on(teamMember.teamMemberPk.email.eq(user.email))
+                    .innerJoin(team).on(teamMember.teamMemberPk.regNum.eq(team.regNum))
+                    .fetchJoin()
+                    .where(teamMember.teamMemberPk.regNum.eq(regNum))
+                    .fetch();
 
         } catch (Exception e) {
             logger.error(GlobalVariable.LOG_PATTERN, this.getClass().getName(), Utils.getStackTrace(e));
